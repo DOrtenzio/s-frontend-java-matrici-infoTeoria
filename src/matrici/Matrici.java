774e8f7c50907365a74e78233f72e74582587551
DOrtenzio <dortenzio.diego.studente@itispaleocapa.it>
2024-04-12 13:07:18 +0200

    Aggiunto stampa: Corretto

--- src/matrici/Matrici.java
+++ src/matrici/Matrici.java
@@ -94,7 +94,7 @@ public class Matrici {
     }
     //Stampa
     public static String Stampa (int [][] m){
-        String s=" ";
+        String s="";
         for (int i = 0; i < m.length; i++) {
             for (int p = 0; p < m[0].length; p++) {
                 s=s+m[i][p]+"\t";
